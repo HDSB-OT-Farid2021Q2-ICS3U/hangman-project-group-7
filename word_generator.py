3577d0e32390eb4de492a880a1f0609e734099b5
Ivan404854 <74551105+Ivan404854@users.noreply.github.com>
2021-01-13 15:23:35 -0500

    finally done appending list at random into py file!

--- word_generator.py
+++ word_generator.py
@@ -1,1 +1,9 @@
+#create word generator for hangman (use list)
 
+import random
+
+wordIndex = random.randrange(0,853)
+
+wordbank = open('hangmanwords.txt', 'r')
+word = (wordbank.readlines()[wordIndex])
+print(word)
