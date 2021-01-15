9f2548e4bd961bb04fd0a7ae0a145ce845a1f8c3
Ivan404854 <74551105+Ivan404854@users.noreply.github.com>
2021-01-15 12:54:27 -0500

    cleaned up code + fixed newline bug

--- word_generator.py
+++ word_generator.py
@@ -1,9 +1,11 @@
-#create word generator for hangman (use list)
 
 import random
 
 wordIndex = random.randrange(0,853)
 
+# open wordbank txt file
 wordbank = open('hangmanwords.txt', 'r')
+# get random line
 word = (wordbank.readlines()[wordIndex])
-print(word)
+# print the line
+print(word, end='') # the word in txt file ends w/ \n so the end does not need to have \n
