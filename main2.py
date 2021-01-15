d4edb1a79b6a112b7290fe8618a61d934671ac1b
Ivan404854 <74551105+Ivan404854@users.noreply.github.com>
2021-01-15 15:33:06 -0500

    cleaned up progrem, fixed a bug

--- main2.py
+++ main2.py
@@ -3,22 +3,23 @@ import random
 
 def win():
     print("congradulations,you beat the game")
+def lose():
+    print(f"you lost the game :( | the correct word was {word}", end='')
 
 wordIndex = random.randrange(0,853)
 
 x = True
 guessbank = ''
+life = 6
 
 # open wordbank txt file
 wordbank = open('hangmanwords.txt', 'r')
 # get random line
 word = (wordbank.readlines()[wordIndex])
-# print the word
-print('reference -', word, end='') # the word in txt file ends w/ \n so the end does not need to have \n
-#=================================
-splitWord = list(word)
-fixedWord = splitWord[:-1]
-#=================================
+# un-comment the line below to see generated world
+#print('reference -', word, end='') # the word in txt file ends w/ \n so the end does not need to have \n
+
+fixedWord = list(word)[:-1]
 
 while x == True:
     for char in fixedWord:
@@ -30,11 +31,19 @@ while x == True:
 
     guess = input('enter guess [letter / word] : ')
     if guess.lower() + '\n' == word:
-        x = False
+        break
         #clear
     else:
-        print('try again')
+        if guess.lower() not in fixedWord:
+            life -= 1
+            if 0 >= life:
+                break
+            #clear
+            print(f'try again | you have {life} lives left!')
         guessbank += guess.lower()
         #clear
 
-win()
+if 0 >= life:
+    lose()
+else:
+    win()
