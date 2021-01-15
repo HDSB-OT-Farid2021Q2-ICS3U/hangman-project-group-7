6c6d93791cdbca70d29bc30ffe038eda84fb4288
HS614 <74550536+HS614@users.noreply.github.com>
2021-01-15 14:00:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ print(word, end='') # the word in txt file ends w/ \n so the end does not need t
 splitWord = list(word)
 fixedWord = splitWord[:-1]
 print(''.join(fixedWord))
+#
+length = len(word)
+blank = []
+for x in range(0,length):
+    blank.append('___')
+print(blank)
+killswitch = True
+while killswitch == True:
+    guess = input("Enter your guess [a letter] : ") # The guess from the player
+    guessL = guess.lower() #convert to lower case so it can take captilazied input
+    position = -1
+    killswitch1 = True
+    while killswitch1 == True:
+        for x in word:
+            position += 1
+            if guessL == x:
+                print("Congradulations, you found a correct characer! ")
+                blank[position] = guessL
+                print(blank)
+            if blank == word:
+                print("congradulations,you beat the game")
+                killswitch = False
