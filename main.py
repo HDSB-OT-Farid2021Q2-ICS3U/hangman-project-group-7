4225a5293e21952bb7704a56834db3cc5805613d
Ivan404854 <74551105+Ivan404854@users.noreply.github.com>
2021-01-15 13:31:56 -0500

    fixed random

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 import random
 
 wordIndex = random.randrange(0,853)
-
+x = True
+guessbank = []
 # open wordbank txt file
 wordbank = open('hangmanwords.txt', 'r')
 # get random line
 word = (wordbank.readlines()[wordIndex])
-# print the line
+# print the word
 print(word, end='') # the word in txt file ends w/ \n so the end does not need to have \n
