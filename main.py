9f1151041eda39f464b92cdab92993fbdb26c3e9
BeevaG <74550899+BeevaG@users.noreply.github.com>
2021-01-15 13:37:14 -0500

    Added code to split the word into a list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import random
 
+
 wordIndex = random.randrange(0,853)
 x = True
 guessbank = []
@@ -10,3 +11,5 @@ wordbank = open('hangmanwords.txt', 'r')
 word = (wordbank.readlines()[wordIndex])
 # print the word
 print(word, end='') # the word in txt file ends w/ \n so the end does not need to have \n
+
+splitWord = list(word)
